8d7a2ac96fefbe544c36d64cedd00e3345a8c1b0
harshit joshi <harshjoshi25022002@gmail.com>
2026-03-13 20:58:38 +0530

    userAlreadyexist or not

---
+++ src/routes/auth.routes.js
@@ -0,0 +1,20 @@
+import express from "express";
+import userModel from "../model/user.model";
+
+const authRouter = express.Router()
+
+authRouter.post("/register",async (req,res)=>{
+    const {username,bio,email, password,profile}=req.body
+
+    const isUserAlreadyExist =await userModel.findOne({
+        $or:[
+            {username},
+            {email}
+        ]
+    })
+    if(isUserAlreadyExist){
+        return res.status(409).json({
+            message:"user is already exist"
+        })
+    }
+})
