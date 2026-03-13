a7a5a503252f43964cbecd5ca5bde011dc89c90f
harshit joshi <harshjoshi25022002@gmail.com>
2026-03-13 21:55:58 +0530

    cookie

--- src/app.js
+++ src/app.js
@@ -1,5 +1,7 @@
 import express from "express"
-
+import cookieParser from "cookie-parser"
 const app = express()
+app.use(express.json())
+app.use(cookieParser())
 
 export default app
--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,5 +1,8 @@
 import express from "express";
 import userModel from "../model/user.model.js";
+import crypto from "crypto"
+import jwt from "jsonwebtoken";
+
 
 const authRouter = express.Router()
 
@@ -17,4 +20,30 @@ authRouter.post("/register",async (req,res)=>{
             message:"user is already exist"
         })
     }
+    const hash = crypto.createHash('sha256').update(password).digest('hex')
+    const user =await userModel.create({
+       username,
+       email,
+       bio,
+       profile,
+       password:hash
+    })
+    const token= jwt.sign({
+        id:user._id
+    },process.env.JWT_SECRET,{expiresIn:"7d"})
+    
+    res.cookie('token',token)
+
+    res.status(201).json({
+        message:"user register successfully",
+        user:{
+            email:user.email,
+            username:user.username,
+            bio:user.bio,
+            profile:user.profile
+        }
+        
+    })
 })
+
+export default authRouter
