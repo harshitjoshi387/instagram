60d839009abe76318934b486437bd5d83a679739
harshit joshi <harshjoshi25022002@gmail.com>
2026-03-15 10:03:31 +0530

    controller

--- src/controller/auth.controller.js
+++ src/controller/auth.controller.js
@@ -1,2 +1,85 @@
 import userModel from "../model/user.model.js";
 import express from "express";
+
+
+
+ async function registerController (req,res){
+    const {username,bio,email, password,profile}=req.body
+
+    const isUserAlreadyExist =await userModel.findOne({
+        $or:[
+            {username},
+            {email}
+        ]
+    })
+    if(isUserAlreadyExist){
+        return res.status(409).json({
+            message:"user is already exist"
+        })
+    }
+   const hash= bcrypt.hash(password,10)
+    const user =await userModel.create({
+       username,
+       email,
+       bio,
+       profile,
+       password:hash
+    })
+    const token= jwt.sign({
+        id:user._id
+    },process.env.JWT_SECRET,{expiresIn:"7d"})
+    
+    res.cookie('token',token)
+
+    res.status(201).json({
+        message:"user register successfully",
+        user:{
+            email:user.email,
+            username:user.username,
+            bio:user.bio,
+            profile:user.profile
+        }
+        
+    })
+}
+
+
+
+
+
+
+
+
+async function loginController(req,res){
+    const {username, email,password}=req.body
+    
+    const user= await userModel.findOne({
+        $or:[
+            {username:username},
+            {email:email}
+        ]
+    })
+    if(!user){
+        return res.status(409).json({
+            message:"user not found"
+        })
+    }
+        
+        const isPasswordValid = await bcrypt.compare(password,user.password)
+
+        if(!isPasswordValid){
+            return res.status(401).json({
+                message:"password invalid"
+            })
+        }
+
+        const token= jwt.sign({
+            id:user._id
+        },process.env.JWT_SECRET,{expiresIn:"7d"})
+
+        res.cookie('token',token)
+
+        res.status(200).json({
+            message:"user loggedin successfully"
+        })
+}
