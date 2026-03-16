714503f25fb6976f2645cea8a40d1e74fb4c442c
harshit joshi <harshjoshi25022002@gmail.com>
2026-03-16 19:42:42 +0530

    chnage

--- src/routes/post.routes.js
+++ src/routes/post.routes.js
@@ -2,6 +2,7 @@ import express from "express";
 import postModel from "../model/post.model";
 import createPostController from "../controller/post.controller";
 import multer from "multer";
+const upload = multer({storage:multer.memoryStorage()})
 const postRouter= express.Router()
 
 
