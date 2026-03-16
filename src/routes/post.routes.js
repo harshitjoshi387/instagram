3813bfbfcc63e290f7be2c5c872690f8826edc47
harshit joshi <harshjoshi25022002@gmail.com>
2026-03-16 19:40:56 +0530

    multer

--- src/routes/post.routes.js
+++ src/routes/post.routes.js
@@ -1,6 +1,7 @@
 import express from "express";
 import postModel from "../model/post.model";
 import createPostController from "../controller/post.controller";
+import multer from "multer";
 const postRouter= express.Router()
 
 
