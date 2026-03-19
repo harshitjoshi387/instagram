f944fafcf2593b06698d0e5d6b779446a11d9a7e
harshit joshi <harshjoshi25022002@gmail.com>
2026-03-19 18:56:23 +0530

    change

--- src/app.js
+++ src/app.js
@@ -7,7 +7,10 @@ import cors from 'cors'
 const app = express()
 app.use(express.json())
 app.use(cookieParser())
-app.use(cors())
+app.use(cors({
+    credentials:true,
+    origin:"http://localhost:5173"
+}))
 
 app.use("/api/auth",authRouter)
 app.use("/api/posts",postRouter)
