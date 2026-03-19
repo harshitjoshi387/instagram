3c1eea5f4b8b67cfd32a9f32bf59cc324f0cbca3
harshit joshi <harshjoshi25022002@gmail.com>
2026-03-19 18:53:13 +0530

    change

--- src/app.js
+++ src/app.js
@@ -2,9 +2,13 @@ import express from "express"
 import cookieParser from "cookie-parser"
 import authRouter from "./routes/auth.routes.js"
 import postRouter from "./routes/post.routes.js"
+import cors from 'cors'
+
 const app = express()
 app.use(express.json())
 app.use(cookieParser())
+app.use(cors())
+
 app.use("/api/auth",authRouter)
 app.use("/api/posts",postRouter)
 export default app
